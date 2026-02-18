33cee346822410cea1dba021154bd5cdce1defe2
faturamento02 <faturamento02@CRISMED.LOCAL>
2026-02-18 15:37:53 -0300

    feat: adiciona validações de requisitos de senha no cadastro
    - Implementa verificação de requisitos (maiúscula, minúscula, número, caractere especial e tamanho)
    - Adiciona feedback visual com ícones de validação em tempo real
    - Inclui verificação de correspondência entre email/email e senha/senha
    - Melhora a experiência do usuário com indicadores claros dos requisitos

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,4 +1,4 @@
-"use client";
+"use client"
 
 import {
   Flex,
@@ -8,84 +8,154 @@ import {
   Input,
   Button,
   Box,
+  List,
   Separator,
   Stack,
   Icon,
   Field,
-} from "@chakra-ui/react";
+} from "@chakra-ui/react"
 
-import {
-  PasswordInput,
-  PasswordStrengthMeter,
-} from "@/components/ui/password-input";
-import BtnGoogle from "./components/BtnGoogle/BtnGoogle";
-import { CiMail } from "react-icons/ci";
-import { PiPassword } from "react-icons/pi";
+import { PasswordInput } from "@/components/ui/password-input"
+import BtnGoogle from "./components/BtnGoogle/BtnGoogle"
+import { CiMail } from "react-icons/ci"
+import { PiPassword } from "react-icons/pi"
+import { MdSmsFailed } from "react-icons/md"
+import { FaCheckCircle } from "react-icons/fa"
 
 import {
   getAuth,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
-} from "firebase/auth";
+} from "firebase/auth"
 
-import useStore from "../../components/globalStates/store";
-import { useRouter } from "next/navigation";
+import useStore from "../../components/globalStates/store"
+import { useRouter } from "next/navigation"
 
-import { useState } from "react";
+import { useState } from "react"
 
 export default function Login() {
-  const [showRegister, setShowRegister] = useState(false);
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmEmail, setConfirmEmail] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
+  const [showRegister, setShowRegister] = useState(false)
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [confirmEmail, setConfirmEmail] = useState("")
+  const [confirmPassword, setConfirmPassword] = useState("")
+  const [emailLogin, setEmailLogin] = useState("")
 
-  const [emailLogin, setEmailLogin] = useState("");
-  const [passwordLogin, setPasswordLogin] = useState("");
+  const [isLengthValid, setIsLengthValid] = useState(false)
+  const [passwordLogin, setPasswordLogin] = useState("")
+  const [hasUppercase, setHasUppercase] = useState(false)
+  const [hasLowercase, setHasLowercase] = useState(false)
+  const [hasSpecialChar, setHasSpecialChar] = useState(false)
+  const [hasNumber, setHasNumber] = useState(false)
 
-  const getUser = useStore((state) => state.getUser);
-  const router = useRouter();
+  const getUser = useStore((state) => state.getUser)
+  const router = useRouter()
+
+  const isPasswordValid =
+    hasUppercase &&
+    hasLowercase &&
+    hasNumber &&
+    hasSpecialChar &&
+    isLengthValid &&
+    password === confirmPassword &&
+    email === confirmEmail
 
   const handleRegister = () => {
-    if (email !== confirmEmail) {
-      alert("Os e-mails informados não coincidem");
-      return;
-    }
+    if (!isPasswordValid) {
+      alert(
+        "Algum requisito de senha não foi cumprido, verifique e tente novamente"
+      )
 
-    if (password !== confirmPassword) {
-      alert("As senhas informadas não coincidem");
-      return;
+      return
     }
-    const auth = getAuth();
+    const auth = getAuth()
     createUserWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
-        const user = userCredential.user;
-        console.log("Usuário criado com sucesso:", user);
-        getUser();
+        const user = userCredential.user
+        console.log("Usuário criado com sucesso:", user)
+        getUser()
 
-        router.push("/dashboard");
+        router.push("/dashboard")
       })
       .catch((Error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        console.log("Erro ao criar usuário:", Error);
-      });
-  };
+        const errorCode = error.code
+        const errorMessage = error.message
+        console.log("Erro ao criar usuário:", Error)
+      })
+  }
+  const requirementPassword = [
+    { requirement: "De 6 a 15 caracteres", valid: isLengthValid },
+    {
+      requirement: "Caractere maiúsculo",
+      valid: hasUppercase,
+    },
+    {
+      requirement: "Caractere minúsculo",
+      valid: hasLowercase,
+    },
+    {
+      requirement: "Caractere especial",
+      valid: hasSpecialChar,
+    },
+    {
+      requirement: "Caractere numérico",
+      valid: hasNumber,
+    },
+    {
+      requirement: "Campos do Email não coincidem",
+      valid: email !== "" && email === confirmEmail,
+      ref: "email",
+    },
+    {
+      requirement: "Campos se senha não coincidem",
+      valid: password !== "" && password === confirmPassword,
+      ref: "password",
+    },
+  ]
+  const checkRequirementPassword = (pass) => {
+    if (/[A-Z]/.test(pass)) {
+      setHasUppercase(true)
+    } else {
+      setHasUppercase(false)
+    }
+
+    if (/[a-z]/.test(pass)) {
+      setHasLowercase(true)
+    } else {
+      setHasLowercase(false)
+    }
+
+    if (/\d/.test(pass)) {
+      setHasNumber(true)
+    } else {
+      setHasNumber(false)
+    }
+    if (/[@#$%&*!?\-_=.,;:]/.test(pass)) {
+      setHasSpecialChar(true)
+    } else {
+      setHasSpecialChar(false)
+    }
+    if (pass.length >= 6 && pass.length <= 15) {
+      setIsLengthValid(true)
+    } else {
+      setIsLengthValid(false)
+    }
+  }
 
   const handleLogin = () => {
-    const auth = getAuth();
+    const auth = getAuth()
     signInWithEmailAndPassword(auth, emailLogin, passwordLogin)
       .then((userCredential) => {
-        const user = userCredential.user;
-        getUser();
+        const user = userCredential.user
+        getUser()
 
-        router.push("/dashboard");
+        router.push("/dashboard")
       })
       .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-      });
-  };
+        const errorCode = error.code
+        const errorMessage = error.message
+      })
+  }
 
   return (
     <>
@@ -289,7 +359,10 @@ export default function Login() {
                   <Field.Label>Senha</Field.Label>
                   <PasswordInput
                     value={password}
-                    onChange={(e) => setPassword(e.target.value)}
+                    onChange={(e) => {
+                      setPassword(e.target.value)
+                      checkRequirementPassword(e.target.value)
+                    }}
                     size={"sm"}
                     placeholder="Senha"
                   />
@@ -299,7 +372,9 @@ export default function Login() {
                   <Field.Label>Confirme sua senha</Field.Label>
                   <PasswordInput
                     value={confirmPassword}
-                    onChange={(e) => setConfirmPassword(e.target.value)}
+                    onChange={(e) => {
+                      setConfirmPassword(e.target.value)
+                    }}
                     _hover={{ borderColor: "gray.500" }}
                     _focus={{
                       borderColor: "primary.500",
@@ -308,9 +383,61 @@ export default function Login() {
                     size={"sm"}
                     color={"gray.600"}
                     placeholder="Confirme sua senha"
+                    // border={}
                   />
                 </Field.Root>
               </Flex>
+
+              <Box fontSize={"xs"}>
+                <Text
+                  fontWeight="semibold"
+                  mb={2}
+                  color="gray.700"
+                  fontSize="sm"
+                >
+                  Requisitos para preenchimento de senha:
+                </Text>{" "}
+                <List.Root fontSize={"xs"} ml={2} unstyled>
+                  {requirementPassword.map((item, index) => (
+                    <List.Item
+                      key={index}
+                      mb={1.5}
+                      display={
+                        (item.ref === "password") & item.valid ||
+                        (item.ref === "email") & item.valid
+                          ? "none"
+                          : "flex"
+                      }
+                    >
+                      <Flex alignItems="center" gap={2}>
+                        <Flex
+                          w="18px"
+                          h="18px"
+                          borderRadius="full"
+                          bg={item.valid ? "green.100" : "red.100"}
+                          align="center"
+                          justify="center"
+                          transition="all 0.2s"
+                        >
+                          <Icon
+                            as={item.valid ? FaCheckCircle : MdSmsFailed}
+                            color={item.valid ? "green.600" : "red.500"}
+                            fontSize="12px"
+                          />
+                        </Flex>
+                        <Text
+                          color={item.valid ? "green.700" : "gray.600"}
+                          fontWeight={item.valid ? "medium" : "normal"}
+                          textDecoration={item.valid ? "none" : "none"}
+                          transition="all 0.2s"
+                        >
+                          {item.requirement}
+                        </Text>
+                      </Flex>
+                    </List.Item>
+                  ))}
+                </List.Root>
+              </Box>
             </Flex>
             <Flex justify={"center"}>
               <Button
@@ -345,5 +472,5 @@ export default function Login() {
         )}
       </Flex>
     </>
-  );
+  )
 }
