3e90837c078ea5d8a74da867c5c06c0d11aedc15
faturamento02 <faturamento02@CRISMED.LOCAL>
2026-02-24 15:16:28 -0300

    feat: Inicia mudanças de estado para result

--- src/app/dashboard/components/BiddingStatusModalEdit/Components/ResultInsertForm/ResultInsertForm.jsx
+++ src/app/dashboard/components/BiddingStatusModalEdit/Components/ResultInsertForm/ResultInsertForm.jsx
@@ -16,75 +16,111 @@ import { FaBoxes } from "react-icons/fa"
 import { TbAlignBoxLeftTopFilled } from "react-icons/tb"
 import { useState } from "react"
 import ChangeButtonResult from "./Components/ChangeButtonResult/ChangeButtonResult"
+import SucessResultForm from "./Components/SucessResultForm/SucessResultForm"
+import LoseResultForm from "./Components/LoseResultForm/LoseResultForm"
+
+import { motion } from "framer-motion"
 
 export default function ResultInsertForm({ open }) {
   const [resultSelectedSucessAtive, setResultSelectedSucessAtive] =
     useState(true)
   const [resultSelectedLoseAtive, setResultSelectedLoseAtive] = useState(false)
 
+  const SlideFromTop = ({ children, delay = 0 }) => {
+    return (
+      <motion.div
+        initial={{ opacity: 0, y: -20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.2, delay }}
+        viewport={{ once: true, margin: "-50px" }}
+      >
+        {children}
+      </motion.div>
+    )
+  }
+
   return (
-    <Dialog.Root open={open} size={"full"}>
-      <Portal>
-        {/* <Dialog.Backdrop /> */}
-        <Dialog.Positioner>
-          <Dialog.Content
-            bg="white"
-            color="gray.800"
-            borderRadius="xl"
-            boxShadow="lg"
-            w={"full"}
-          >
-            <Dialog.Header>
-              <Flex align={"center"} gap={3}>
-                <Icon size={"xl"} colorPalette={"blue"} color={"blue.600"}>
-                  <Dialog.CloseTrigger top="0" asChild>
-                    <CloseButton bg="bg" size="sm" />
-                  </Dialog.CloseTrigger>
-                  <TbAlignBoxLeftTopFilled />
-                </Icon>
-                <Dialog.Title>Inserção de Resultado</Dialog.Title>
-              </Flex>
-            </Dialog.Header>
-            <Dialog.Body>
-              <Flex
-                pt={4}
-                justifyContent={"center"}
-                alignItems={"center"}
-                gap={3}
+    <>
+      <SlideFromTop>
+        <Dialog.Root open={open} size={"full"}>
+          <Portal>
+            {/* <Dialog.Backdrop /> */}
+            <Dialog.Positioner>
+              <Dialog.Content
+                bg="white"
+                color="gray.800"
+                borderRadius="xl"
+                boxShadow="lg"
+                w={"full"}
               >
-                <ChangeButtonResult
-                  onClick={() => {
-                    setResultSelectedSucessAtive(!resultSelectedSucessAtive)
-                    {
-                      if (resultSelectedLoseAtive) {
-                        setResultSelectedLoseAtive(false)
-                      }
-                    }
-                  }}
-                  type={"sucess"}
-                  changeResultTitle={"Processo ganho"}
-                  ative={resultSelectedSucessAtive}
-                />
+                <Dialog.Header>
+                  <Flex align={"center"} gap={3}>
+                    <Icon size={"xl"} colorPalette={"blue"} color={"blue.600"}>
+                      <Dialog.CloseTrigger top="0" asChild>
+                        <CloseButton bg="bg" size="sm" />
+                      </Dialog.CloseTrigger>
+                      <TbAlignBoxLeftTopFilled />
+                    </Icon>
+                    <Dialog.Title>Inserção de Resultado</Dialog.Title>
+                  </Flex>
+                </Dialog.Header>
+                <Dialog.Body>
+                  <Flex
+                    pt={4}
+                    justifyContent={"center"}
+                    alignItems={"center"}
+                    gap={3}
+                  >
+                    <ChangeButtonResult
+                      onClick={() => {
+                        setResultSelectedSucessAtive(!resultSelectedSucessAtive)
+                        {
+                          if (resultSelectedLoseAtive) {
+                            setResultSelectedLoseAtive(false)
+                          }
+                        }
+                      }}
+                      type={"sucess"}
+                      changeResultTitle={"Processo ganho"}
+                      ative={resultSelectedSucessAtive}
+                    />
 
-                <ChangeButtonResult
-                  type={"lose"}
-                  changeResultTitle={"Processo perdido"}
-                  onClick={() => {
-                    setResultSelectedLoseAtive(!resultSelectedLoseAtive)
+                    <ChangeButtonResult
+                      type={"lose"}
+                      changeResultTitle={"Processo perdido"}
+                      onClick={() => {
+                        setResultSelectedLoseAtive(!resultSelectedLoseAtive)
 
-                    {
-                      if (resultSelectedSucessAtive) {
-                        setResultSelectedSucessAtive(false)
-                      }
-                    }
-                  }}
-                  ative={resultSelectedLoseAtive}
-                />
-              </Flex>
-            </Dialog.Body>
-          </Dialog.Content>
-        </Dialog.Positioner>
-      </Portal>{" "}
-    </Dialog.Root>
+                        {
+                          if (resultSelectedSucessAtive) {
+                            setResultSelectedSucessAtive(false)
+                          }
+                        }
+                      }}
+                      ative={resultSelectedLoseAtive}
+                    />
+                  </Flex>
+                  {resultSelectedSucessAtive && (
+                    <Flex>
+                      <SlideFromTop>
+                        <SucessResultForm />
+                      </SlideFromTop>
+                    </Flex>
+                  )}
+
+                  {resultSelectedLoseAtive && (
+                    <Flex>
+                      <SlideFromTop>
+                        <LoseResultForm />
+                      </SlideFromTop>
+                    </Flex>
+                  )}
+                </Dialog.Body>
+              </Dialog.Content>
+            </Dialog.Positioner>
+          </Portal>{" "}
+        </Dialog.Root>
+      </SlideFromTop>
+    </>
   )
 }
